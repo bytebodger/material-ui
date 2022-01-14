91c5e9d186d3c5ebb82d688e747b5e5e9d4dca73
Adam Nathaniel Davis <adamdavis@filevine.com>
2022-01-13 22:44:35 -0500

    converted back to React.Fragment

--- src/Column.js
+++ src/Column.js
@@ -10,7 +10,7 @@ export const Column = props => {
       >
          {props.children}
       </Grid>
-   </>;
+   </React.Fragment>;
 };
 
 const columnOptions = [false, 'auto', true, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
