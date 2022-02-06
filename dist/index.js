be1444fafc08a224e4807f31b79d4b9db1ad4bee
Adam Nathaniel Davis <adamdavis@filevine.com>
2022-02-05 19:13:20 -0500

    converting to React app w/dist folder

---
+++ dist/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Column", {
+  enumerable: true,
+  get: function get() {
+    return _Column.Column;
+  }
+});
+Object.defineProperty(exports, "Row", {
+  enumerable: true,
+  get: function get() {
+    return _Row.Row;
+  }
+});
+
+var _Row = require("./components/Row");
+
+var _Column = require("./components/Column");
